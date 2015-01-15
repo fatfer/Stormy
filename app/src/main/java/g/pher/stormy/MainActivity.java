1b350cc4f0f6293f918fa8733d01defdb8ca8a04
Fer <pher.gm@gmail.com>
2015-01-15 21:39:19 +0100

    asynchronous GET api request

--- app/src/main/java/g/pher/stormy/MainActivity.java
+++ app/src/main/java/g/pher/stormy/MainActivity.java
@@ -1,17 +1,58 @@
 package g.pher.stormy;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "5a3fdd4552db32339e3c0720a08cd102";
+        String latitude = "37.8267";
+        String longitude = "-122.423";
+        String forecastUrl = "https://api.forecast.io/forecast/" + apiKey +
+                "/" + latitude + "," + longitude;
+
+        Request request = new Request.Builder()
+                .url(forecastUrl)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                Log.e(TAG, "Error", e);
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                if (!response.isSuccessful()) {
+                    throw new IOException("Unexpected code " + response);
+                }
+                System.out.println(response.body().string());
+            }
+        });
+
+
     }
 
 
