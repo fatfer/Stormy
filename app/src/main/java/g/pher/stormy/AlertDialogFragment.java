3f6e348c27b21369e4538f33aec3d6eea151fd32
Fer <pher.gm@gmail.com>
2015-01-17 20:50:47 +0100

    extract strings to resources

--- app/src/main/java/g/pher/stormy/AlertDialogFragment.java
+++ app/src/main/java/g/pher/stormy/AlertDialogFragment.java
@@ -15,9 +15,9 @@ public class AlertDialogFragment extends DialogFragment{
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Context context = getActivity();
         AlertDialog.Builder builder = new AlertDialog.Builder(context)
-                .setTitle("Sorry!")
-                .setMessage("There was an error. Please try again.")
-                .setPositiveButton("Ok", null);
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
 
         AlertDialog dialog = builder.create();
         return dialog;
